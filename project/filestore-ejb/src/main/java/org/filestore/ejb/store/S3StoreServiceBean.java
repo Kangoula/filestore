407c8a906416af7372237d924712fac52aa81bdc
Alexandre GINGEMBRE <alexandre.gingembre@sfr.fr>
2016-11-24 12:48:30 +0100

    bug fix and clean LOG

--- project/filestore-ejb/src/main/java/org/filestore/ejb/store/S3StoreServiceBean.java
+++ project/filestore-ejb/src/main/java/org/filestore/ejb/store/S3StoreServiceBean.java
@@ -14,8 +14,6 @@ import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -53,61 +51,23 @@ public class S3StoreServiceBean implements S3StoreService {
     }
 
     public String put(FileData data) throws BinaryStoreServiceException {
-        List<PartETag> partETags = new ArrayList<PartETag>();
         String id = UUID.randomUUID().toString().replaceAll("-", "");
         InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(
                 bucketName, id);
         InitiateMultipartUploadResult initResponse =
                 client.initiateMultipartUpload(initRequest);
-
-       // File file = new File(filePath);
-        long partSize = 5 * 1024 * 1024; // Set part size to 5 MB.
         LOGGER.log(Level.INFO, "Starting upload");
         try {
 
             ObjectMetadata m = new ObjectMetadata();
             m.setContentDisposition("attachment; filename="+data.getName());
-          //  PutObjectRequest p = new PutObjectRequest(bucketName, id, data.getData().getInputStream(), m)
-          //          .withCannedAcl(CannedAccessControlList.PublicRead);
-          //  p.getMetadata().setContentLength(data.getSize());
             TransferManager t = new TransferManager(cred);
-            //m.setContentLength(data.getSize());
-            Upload caca = t.upload(bucketName, id, data.getData().getInputStream(),m);
-            LOGGER.log(Level.INFO, "MAROT : Transfert finish");
-            caca.waitForCompletion();
+            Upload fileUpload = t.upload(new PutObjectRequest(bucketName, id, data.getData().getInputStream(), m)
+                             .withCannedAcl(CannedAccessControlList.PublicRead));
+            LOGGER.log(Level.INFO, "Transfert finish");
+            fileUpload.waitForCompletion();
             t.shutdownNow();
-
-           LOGGER.log(Level.INFO, "PROUT " + caca.getState().toString());
-
-           /* // Step 2: Upload parts.
-            long filePosition = 0;
-            for (int i = 1; filePosition < data.getSize(); i++) {
-                // Last part can be less than 5 MB. Adjust part size.
-                partSize = Math.min(partSize, (data.getSize() - filePosition));
-                LOGGER.log(Level.INFO, "PART " + i);
-                // Create request to upload a part.
-                UploadPartRequest uploadRequest = new UploadPartRequest()
-                        .withBucketName(bucketName).withKey(id)
-                        .withUploadId(initResponse.getUploadId()).withPartNumber(i)
-                        .withFileOffset(filePosition)
-                        .withInputStream(data.getData().getInputStream())
-                        .withPartSize(partSize);
-
-                // Upload part and add response to our list.
-                partETags.add(client.uploadPart(uploadRequest).getPartETag());
-                LOGGER.log(Level.INFO, "Upload request send");
-                filePosition += partSize;
-            }
-            LOGGER.log(Level.INFO, "Boucle finie");*/
-            // Step 3: Complete.
-            CompleteMultipartUploadRequest compRequest = new
-                    CompleteMultipartUploadRequest(bucketName,
-                    id,
-                    initResponse.getUploadId(),
-                    partETags);
-
-            client.completeMultipartUpload(compRequest);
-            LOGGER.log(Level.INFO, "Requête finie");
+            LOGGER.log(Level.INFO, "End request");
         } catch (Exception e) {
             LOGGER.log(Level.INFO, "Abort upload " + e.getMessage());
             client.abortMultipartUpload(new AbortMultipartUploadRequest(
